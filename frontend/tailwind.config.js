95f9d949a437aeaae716213d5c605421da78e8f0
Tristan Alonso <tristanalonso09@gmail.com>
2024-05-23 17:33:46 +0200

    chore: Update tailwind.config.js with Material Tailwind integration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+/** @type {(tailwindConfig: object) => object} */
+const withMt = require('@material-tailwind/react/utils/withMT')
+module.exports = withMt({
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {}
   },
   plugins: []
-}
+})
