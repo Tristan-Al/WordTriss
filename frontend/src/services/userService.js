244e9dff7dc9fd435665cd774c3d4be012497242
Tristan Alonso <tristanalonso09@gmail.com>
2024-05-27 15:26:26 +0200

    refactor: Update userService to handle user updates and authorization checks

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -28,6 +28,60 @@ const userService = {
       )
       throw error
     }
+  },
+
+  updateUser: async (user, curUserId, curUserRoleName) => {
+    try {
+      // Check if the user is an admin or the user is not updating itself
+      if (curUserId !== user.id && curUserRoleName !== 'ADMIN') {
+        return {
+          ok: false,
+          message: 'You are not authorized to update this user'
+        }
+      }
+
+      // Change format of the fields
+      user.display_name = user.displayName
+      delete user.displayName
+      delete user.roleId
+
+      //Check if any field is empty
+      if (!user.display_name || !user.username || !user.email) {
+        return {
+          ok: false,
+          message: 'Necessary fields are required'
+        }
+      }
+
+      // Check if the user is updating password
+      if (user.password === '') {
+        // Check if confirm password is empty
+        if (user.confirmPassword === '') {
+          return {
+            ok: false,
+            message: 'Confirm password is required'
+          }
+        }
+
+        // Check if the password and confirm password match
+        if (user.password !== user.confirmPassword) {
+          return {
+            ok: false,
+            message: 'Passwords do not match'
+          }
+        }
+
+        // Change the format of the password
+        user.confirm_password = user.confirmPassword
+        delete user.confirmPassword
+      }
+
+      // Make the request to the api
+      return await api.put(`users/${user.id}`, user)
+    } catch (error) {
+      console.error('Error updating user:', error.message)
+      throw error
+    }
   }
 }
 
